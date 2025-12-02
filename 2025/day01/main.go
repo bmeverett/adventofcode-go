8aa95b4e913858d89a752b7e33e1126b388fc8da
Brandon Everett <bme2010@gmail.com>
2025-12-01 22:40:33 -0500

    2025 day 1 part 2 wip

--- 2025/day01/main.go
+++ 2025/day01/main.go
@@ -24,30 +24,40 @@ func run(input string, part int) int {
 	lines := strings.Split(input, "\n")
 	dial := 50
 	zeroCount := 0
+	zeroDial := false
 	for _, l := range lines {
 		splt := strings.Split(l, "")
 		dir := splt[0]
 		num, _ := strconv.Atoi(strings.Join(splt[1:], ""))
 
 		if dir == "L" {
-			//fmt.Printf("L %d\n", num)
+			fmt.Printf("L %d\n", num)
 			dial -= num
 			for dial < 0 {
 				dial += 100
+				if part == 2 && !zeroDial && dial != 0 {
+					zeroCount++
+				}
 			}
 
 		} else if dir == "R" {
 			dial += num
-			//fmt.Printf("R %d\n", num)
+			fmt.Printf("R %d\n", num)
 			for dial > 99 {
 				dial -= 100
+				if part == 2 && !zeroDial && dial != 0 {
+					zeroCount++
+				}
 			}
 		}
 
-		//	fmt.Printf("Dial %d\n", dial)
-		//284 low
+		fmt.Printf("Dial %d\nZero: %d\n", dial, zeroCount)
+		//6659 low
 		if dial == 0 {
 			zeroCount++
+			zeroDial = true
+		} else {
+			zeroDial = false
 		}
 
 	}
